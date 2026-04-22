39f4f761ce1a2fa03002cb633bef2421d9d6905a
François Lami <lamifrancois@gmail.com>
2026-04-22 02:26:06 +0200

    feat(ui): rewrite SoundTab with new atoms + layoutFor(BP) (Phase 2 Task 10)

--- Source/UI/SoundTab.h
+++ Source/UI/SoundTab.h
@@ -1,164 +1,166 @@
 #pragma once
 #include <juce_audio_processors/juce_audio_processors.h>
-#include "KnobWithLabel.h"
-#include "LookAndFeel.h"
+#include "Breakpoint.h"
+#include "DesignTokens.h"
 #include "../Parameters/ParameterIDs.h"
+#include "components/ArcKnob.h"
+#include "components/EnvelopeEditor.h"
+#include "components/FilterResponseView.h"
+#include "components/FilterTypeSelect.h"
+#include "components/OscilloscopeView.h"
+#include "components/PanelHeader.h"
+#include "components/WaveTypeSelect.h"
 
 class SoundTab : public juce::Component
 {
 public:
     explicit SoundTab(juce::AudioProcessorValueTreeState& apvts)
-        : apvts(apvts)
+        : apvts(apvts),
+          osc1Header("OSC 1", SynthVibe::Tokens::osc),
+          osc2Header("OSC 2", SynthVibe::Tokens::osc),
+          filterHeader("FILTER", SynthVibe::Tokens::filter),
+          ampEnvHeader("AMP ENV", SynthVibe::Tokens::env),
+          fltEnvHeader("FLT ENV", SynthVibe::Tokens::env),
+          osc1Wave(apvts, ParamIDs::osc1Waveform),
+          osc2Wave(apvts, ParamIDs::osc2Waveform),
+          filterType(apvts, ParamIDs::filterType),
+          osc1Scope(apvts, ParamIDs::osc1Waveform),
+          osc2Scope(apvts, ParamIDs::osc2Waveform),
+          filterResponse(apvts,
+                         ParamIDs::filterType,
+                         ParamIDs::filterCutoff,
+                         ParamIDs::filterResonance),
+          ampEnv(apvts, ParamIDs::ampAttack, ParamIDs::ampDecay,
+                 ParamIDs::ampSustain, ParamIDs::ampRelease),
+          fltEnv(apvts, ParamIDs::fltAttack, ParamIDs::fltDecay,
+                 ParamIDs::fltSustain, ParamIDs::fltRelease),
+          knobOsc1Oct("Oct", apvts, ParamIDs::osc1Octave, SynthVibe::Tokens::osc, "", 0),
+          knobOsc1Semi("Semi", apvts, ParamIDs::osc1Semitone, SynthVibe::Tokens::osc, "", 0),
+          knobOsc1Detune("Detune", apvts, ParamIDs::osc1Detune, SynthVibe::Tokens::osc, " ct", 1),
+          knobOsc1Level("Level", apvts, ParamIDs::osc1Level, SynthVibe::Tokens::osc, "", 2),
+          knobUniVoices("Voices", apvts, ParamIDs::unisonVoices, SynthVibe::Tokens::osc, "", 0),
+          knobUniDetune("Detune", apvts, ParamIDs::unisonDetune, SynthVibe::Tokens::osc, " ct", 1),
+          knobUniSpread("Stereo", apvts, ParamIDs::unisonStereoSpread, SynthVibe::Tokens::osc, "", 2),
+          knobOsc2Oct("Oct", apvts, ParamIDs::osc2Octave, SynthVibe::Tokens::osc, "", 0),
+          knobOsc2Semi("Semi", apvts, ParamIDs::osc2Semitone, SynthVibe::Tokens::osc, "", 0),
+          knobOsc2Detune("Detune", apvts, ParamIDs::osc2Detune, SynthVibe::Tokens::osc, " ct", 1),
+          knobOsc2Level("Level", apvts, ParamIDs::osc2Level, SynthVibe::Tokens::osc, "", 2),
+          knobCutoff("Cutoff", apvts, ParamIDs::filterCutoff, SynthVibe::Tokens::filter, " Hz", 0),
+          knobResonance("Res", apvts, ParamIDs::filterResonance, SynthVibe::Tokens::filter, "", 2),
+          knobFilterEnv("Env", apvts, ParamIDs::filterEnvAmt, SynthVibe::Tokens::filter, "", 2)
     {
-        osc1WaveBox.addItemList({ "Sine", "Saw", "Square", "Triangle" }, 1);
-        osc1WaveAttach = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment>(
-            apvts, ParamIDs::osc1Waveform, osc1WaveBox);
-
-        osc2WaveBox.addItemList({ "Sine", "Saw", "Square", "Triangle" }, 1);
-        osc2WaveAttach = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment>(
-            apvts, ParamIDs::osc2Waveform, osc2WaveBox);
-
-        filterTypeBox.addItemList({ "Low Pass", "High Pass", "Band Pass" }, 1);
-        filterTypeAttach = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment>(
-            apvts, ParamIDs::filterType, filterTypeBox);
-
-        for (auto* c : { &osc1WaveBox, &osc2WaveBox, &filterTypeBox })
+        for (auto* c : std::initializer_list<juce::Component*> {
+            &osc1Header, &osc2Header, &filterHeader, &ampEnvHeader, &fltEnvHeader,
+            &osc1Wave, &osc2Wave, &filterType,
+            &osc1Scope, &osc2Scope, &filterResponse,
+            &ampEnv, &fltEnv,
+            &knobOsc1Oct, &knobOsc1Semi, &knobOsc1Detune, &knobOsc1Level,
+            &knobUniVoices, &knobUniDetune, &knobUniSpread,
+            &knobOsc2Oct, &knobOsc2Semi, &knobOsc2Detune, &knobOsc2Level,
+            &knobCutoff, &knobResonance, &knobFilterEnv
+        })
             addAndMakeVisible(c);
-
-        addAndMakeVisible(knobOsc1Oct);
-        addAndMakeVisible(knobOsc1Semi);
-        addAndMakeVisible(knobOsc1Detune);
-        addAndMakeVisible(knobOsc1Level);
-        addAndMakeVisible(knobUnisonVoices);
-        addAndMakeVisible(knobUnisonSpread);
-        addAndMakeVisible(knobUnisonStereo);
-        addAndMakeVisible(knobOsc2Oct);
-        addAndMakeVisible(knobOsc2Semi);
-        addAndMakeVisible(knobOsc2Detune);
-        addAndMakeVisible(knobOsc2Level);
-        addAndMakeVisible(knobCutoff);
-        addAndMakeVisible(knobResonance);
-        addAndMakeVisible(knobFilterEnv);
-        addAndMakeVisible(knobAmpA);
-        addAndMakeVisible(knobAmpD);
-        addAndMakeVisible(knobAmpS);
-        addAndMakeVisible(knobAmpR);
-        addAndMakeVisible(knobFltA);
-        addAndMakeVisible(knobFltD);
-        addAndMakeVisible(knobFltS);
-        addAndMakeVisible(knobFltR);
     }
 
     void paint(juce::Graphics& g) override
     {
-        drawPanel(g, osc1Bounds,   "OSC 1",   SynthLookAndFeel::colOscAccent);
-        drawPanel(g, osc2Bounds,   "OSC 2",   SynthLookAndFeel::colOscAccent);
-        drawPanel(g, filterBounds, "FILTER",  SynthLookAndFeel::colFilterAccent);
-        drawPanel(g, ampEnvBounds, "AMP ENV", SynthLookAndFeel::colEnvAccent);
-        drawPanel(g, fltEnvBounds, "FLT ENV", SynthLookAndFeel::colEnvAccent);
+        using namespace SynthVibe::Tokens;
+        for (auto r : { osc1Bounds, osc2Bounds, filterBounds, ampEnvBounds, fltEnvBounds })
+        {
+            auto f = r.toFloat().reduced(2.f);
+            g.setColour(panel);
+            g.fillRoundedRectangle(f, radiusMd);
+            g.setColour(edge);
+            g.drawRoundedRectangle(f, radiusMd, 1.f);
+        }
     }
 
-    void resized() override
+    void resized() override { layoutFor(SynthVibe::breakpointForWidth(getWidth())); }
+
+    void layoutFor(SynthVibe::BP /*bp*/)
     {
-        auto area = getLocalBounds().reduced(6);
-        const int pad    = 6;
-        const int comboH = 26;
-        const int titleH = 20;
+        using namespace SynthVibe::Tokens;
+        const int headerH = 20;
+        const int scopeH  = 60;
+        const int selectH = 26;
 
-        const int col3W = area.getWidth() / 3;
-        auto col1 = area.removeFromLeft(col3W).reduced(pad, 0);
-        auto col2 = area.removeFromLeft(col3W).reduced(pad, 0);
-        auto col3 = area.reduced(pad, 0);
+        auto area = getLocalBounds().reduced(spaceSm);
+        const int colW = area.getWidth() / 3;
+        auto col1 = area.removeFromLeft(colW).reduced(spaceXs, 0);
+        auto col2 = area.removeFromLeft(colW).reduced(spaceXs, 0);
+        auto col3 = area.reduced(spaceXs, 0);
 
-        // OSC 1
         osc1Bounds = col1;
-        auto c1 = col1.withTrimmedTop(titleH);
-        osc1WaveBox.setBounds(c1.removeFromTop(comboH));
-        auto oscRow = c1.removeFromTop(c1.getHeight() / 2);
-        layoutKnobs(oscRow, { &knobOsc1Oct, &knobOsc1Semi, &knobOsc1Detune, &knobOsc1Level });
-        layoutKnobs(c1, { &knobUnisonVoices, &knobUnisonSpread, &knobUnisonStereo });
+        auto c1 = col1.reduced(spaceSm);
+        osc1Header.setBounds(c1.removeFromTop(headerH));
+        c1.removeFromTop(spaceXs);
+        osc1Scope.setBounds(c1.removeFromTop(scopeH));
+        c1.removeFromTop(spaceXs);
+        osc1Wave.setBounds(c1.removeFromTop(selectH));
+        c1.removeFromTop(spaceSm);
+        auto oscRow1 = c1.removeFromTop(c1.getHeight() / 2);
+        layoutKnobsRow(oscRow1, { &knobOsc1Oct, &knobOsc1Semi, &knobOsc1Detune, &knobOsc1Level });
+        layoutKnobsRow(c1, { &knobUniVoices, &knobUniDetune, &knobUniSpread });
 
-        // OSC 2
         osc2Bounds = col2;
-        auto c2 = col2.withTrimmedTop(titleH);
-        osc2WaveBox.setBounds(c2.removeFromTop(comboH));
-        layoutKnobs(c2, { &knobOsc2Oct, &knobOsc2Semi, &knobOsc2Detune, &knobOsc2Level });
+        auto c2 = col2.reduced(spaceSm);
+        osc2Header.setBounds(c2.removeFromTop(headerH));
+        c2.removeFromTop(spaceXs);
+        osc2Scope.setBounds(c2.removeFromTop(scopeH));
+        c2.removeFromTop(spaceXs);
+        osc2Wave.setBounds(c2.removeFromTop(selectH));
+        c2.removeFromTop(spaceSm);
+        layoutKnobsRow(c2, { &knobOsc2Oct, &knobOsc2Semi, &knobOsc2Detune, &knobOsc2Level });
 
-        // Col 3 stacked
-        const int filterH = col3.getHeight() * 35 / 100;
+        const int filterH = col3.getHeight() * 38 / 100;
         const int envH    = (col3.getHeight() - filterH) / 2;
 
         filterBounds = col3.removeFromTop(filterH);
-        auto fc = filterBounds.withTrimmedTop(titleH);
-        filterTypeBox.setBounds(fc.removeFromTop(comboH));
-        layoutKnobs(fc, { &knobCutoff, &knobResonance, &knobFilterEnv });
+        auto cf = filterBounds.reduced(spaceSm);
+        filterHeader.setBounds(cf.removeFromTop(headerH));
+        cf.removeFromTop(spaceXs);
+        filterResponse.setBounds(cf.removeFromTop(cf.getHeight() / 2));
+        cf.removeFromTop(spaceXs);
+        filterType.setBounds(cf.removeFromTop(selectH));
+        cf.removeFromTop(spaceXs);
+        layoutKnobsRow(cf, { &knobCutoff, &knobResonance, &knobFilterEnv });
 
         ampEnvBounds = col3.removeFromTop(envH);
-        layoutKnobs(ampEnvBounds.withTrimmedTop(titleH),
-                    { &knobAmpA, &knobAmpD, &knobAmpS, &knobAmpR });
+        auto ca = ampEnvBounds.reduced(spaceSm);
+        ampEnvHeader.setBounds(ca.removeFromTop(headerH));
+        ca.removeFromTop(spaceXs);
+        ampEnv.setBounds(ca);
 
         fltEnvBounds = col3;
-        layoutKnobs(fltEnvBounds.withTrimmedTop(titleH),
-                    { &knobFltA, &knobFltD, &knobFltS, &knobFltR });
+        auto cfe = fltEnvBounds.reduced(spaceSm);
+        fltEnvHeader.setBounds(cfe.removeFromTop(headerH));
+        cfe.removeFromTop(spaceXs);
+        fltEnv.setBounds(cfe);
     }
 
 private:
+    static void layoutKnobsRow(juce::Rectangle<int> bounds,
+                               std::initializer_list<juce::Component*> knobs)
+    {
+        if (knobs.size() == 0) return;
+        const int w = bounds.getWidth() / (int) knobs.size();
+        auto b = bounds;
+        for (auto* k : knobs) k->setBounds(b.removeFromLeft(w));
+    }
+
     juce::AudioProcessorValueTreeState& apvts;
 
     juce::Rectangle<int> osc1Bounds, osc2Bounds, filterBounds, ampEnvBounds, fltEnvBounds;
 
-    juce::ComboBox osc1WaveBox, osc2WaveBox, filterTypeBox;
-    std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment>
-        osc1WaveAttach, osc2WaveAttach, filterTypeAttach;
+    SynthVibe::PanelHeader       osc1Header, osc2Header, filterHeader, ampEnvHeader, fltEnvHeader;
+    SynthVibe::WaveTypeSelect    osc1Wave, osc2Wave;
+    SynthVibe::FilterTypeSelect  filterType;
+    SynthVibe::OscilloscopeView  osc1Scope, osc2Scope;
+    SynthVibe::FilterResponseView filterResponse;
+    SynthVibe::EnvelopeEditor    ampEnv, fltEnv;
 
-    KnobWithLabel knobOsc1Oct    { "Oct",    apvts, ParamIDs::osc1Octave,    "",    0 };
-    KnobWithLabel knobOsc1Semi   { "Semi",   apvts, ParamIDs::osc1Semitone,  "",    0 };
-    KnobWithLabel knobOsc1Detune { "Detune", apvts, ParamIDs::osc1Detune,    " ct", 1 };
-    KnobWithLabel knobOsc1Level  { "Level",  apvts, ParamIDs::osc1Level,     "",    2 };
-    KnobWithLabel knobUnisonVoices { "Voices", apvts, ParamIDs::unisonVoices,       "",    0 };
-    KnobWithLabel knobUnisonSpread { "Detune", apvts, ParamIDs::unisonDetune,       " ct", 1 };
-    KnobWithLabel knobUnisonStereo { "Stereo", apvts, ParamIDs::unisonStereoSpread, "",    2 };
-
-    KnobWithLabel knobOsc2Oct    { "Oct",    apvts, ParamIDs::osc2Octave,    "",    0 };
-    KnobWithLabel knobOsc2Semi   { "Semi",   apvts, ParamIDs::osc2Semitone,  "",    0 };
-    KnobWithLabel knobOsc2Detune { "Detune", apvts, ParamIDs::osc2Detune,    " ct", 1 };
-    KnobWithLabel knobOsc2Level  { "Level",  apvts, ParamIDs::osc2Level,     "",    2 };
-
-    KnobWithLabel knobCutoff    { "Cutoff",  apvts, ParamIDs::filterCutoff,    " Hz", 0 };
-    KnobWithLabel knobResonance { "Res",     apvts, ParamIDs::filterResonance, "",    2 };
-    KnobWithLabel knobFilterEnv { "Env Amt", apvts, ParamIDs::filterEnvAmt,    "",    2 };
-
-    KnobWithLabel knobAmpA { "A", apvts, ParamIDs::ampAttack,  " s", 3 };
-    KnobWithLabel knobAmpD { "D", apvts, ParamIDs::ampDecay,   " s", 3 };
-    KnobWithLabel knobAmpS { "S", apvts, ParamIDs::ampSustain, "",   2 };
-    KnobWithLabel knobAmpR { "R", apvts, ParamIDs::ampRelease, " s", 3 };
-
-    KnobWithLabel knobFltA { "A", apvts, ParamIDs::fltAttack,  " s", 3 };
-    KnobWithLabel knobFltD { "D", apvts, ParamIDs::fltDecay,   " s", 3 };
-    KnobWithLabel knobFltS { "S", apvts, ParamIDs::fltSustain, "",   2 };
-    KnobWithLabel knobFltR { "R", apvts, ParamIDs::fltRelease, " s", 3 };
-
-    static void drawPanel(juce::Graphics& g, juce::Rectangle<int> bounds,
-                          const juce::String& title, juce::uint32 accentColour)
-    {
-        auto f = bounds.toFloat().reduced(2.f);
-        g.setColour(juce::Colour(SynthLookAndFeel::colPanel));
-        g.fillRoundedRectangle(f, 6.f);
-        g.setColour(juce::Colour(accentColour));
-        g.drawRoundedRectangle(f, 6.f, 1.f);
-        g.setColour(juce::Colour(SynthLookAndFeel::colHighlight));
-        g.setFont(juce::Font(11.f, juce::Font::bold));
-        g.drawText(title, bounds.getX() + 10, bounds.getY() + 4,
-                   bounds.getWidth() - 20, 16, juce::Justification::centredLeft);
-    }
-
-    static void layoutKnobs(juce::Rectangle<int> bounds,
-                            std::initializer_list<juce::Component*> knobs)
-    {
-        if (knobs.size() == 0) return;
-        const int w = bounds.getWidth() / static_cast<int>(knobs.size());
-        auto b = bounds;
-        for (auto* k : knobs)
-            k->setBounds(b.removeFromLeft(w));
-    }
+    SynthVibe::ArcKnob knobOsc1Oct, knobOsc1Semi, knobOsc1Detune, knobOsc1Level;
+    SynthVibe::ArcKnob knobUniVoices, knobUniDetune, knobUniSpread;
+    SynthVibe::ArcKnob knobOsc2Oct, knobOsc2Semi, knobOsc2Detune, knobOsc2Level;
+    SynthVibe::ArcKnob knobCutoff, knobResonance, knobFilterEnv;
 };
